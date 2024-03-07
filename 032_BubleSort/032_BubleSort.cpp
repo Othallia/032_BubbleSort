2449c15a3127dbf66c7ae6d6867cde1f43ed74a1
Othallia <vianiputriocta6@gmail.com>
2024-03-07 19:50:34 +0700

    buat variable global

--- 032_BubleSort/032_BubleSort.cpp
+++ 032_BubleSort/032_BubleSort.cpp
@@ -1,3 +1,6 @@
 #include <iostream>
+using namespace std;
 
-int main();
+int arr[20];				// Deklarasi array a dengan ukuran 20
+int n;						// Deklarasi variabel n untuk menyimpan banyaknya elemen
+
